c8c7664dd46b23e337f321162f971daec36da5db
Thiago Durante Pires <thdurante@gmail.com>
2017-07-16 11:28:22 -0300

    Configuring Amazon S3 for Paperclip uploads

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,19 @@ module HeBlog
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.default_locale = 'pt-BR'
+
+    # Amazon S3 settings for Paperclip uploads
+    config.paperclip_defaults = {
+        storage: :s3,
+        s3_protocol: 'https',
+        url: ':s3_domain_url',
+        path: '/:class/:attachment/:id_partition/:style/:filename',
+        s3_credentials: {
+            bucket: ENV.fetch('S3_BUCKET_NAME'),
+            access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+            secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+            s3_region: ENV.fetch('AWS_REGION'),
+        }
+    }
   end
 end
