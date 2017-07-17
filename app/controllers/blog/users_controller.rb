4eb864e6a521bea4181ab3955534186895a4df6c
Thiago Durante Pires <thdurante@gmail.com>
2017-07-17 19:00:50 -0300

    Creating blog/users 'show' view

--- app/controllers/blog/users_controller.rb
+++ app/controllers/blog/users_controller.rb
@@ -4,6 +4,8 @@ module Blog
     before_action :set_user, only: [:show]
 
     def show
+      @post = Post.new
+      @posts = Post.where(user_id: @user.id).paginate(page: params[:page], per_page: 4)
     end
 
     private
