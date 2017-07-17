b9ea43c23e1677a65c57a822d0f5c0a1e1b03fc0
Thiago Durante Pires <thdurante@gmail.com>
2017-07-16 23:59:30 -0300

    Change default_url for paperclip config

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_attached_file :avatar,
                     styles: { thumb: '160x160#' },
-                    default_url: '/:class/:attachment/default/:style/:filename'
+                    default_url: '/images/:attachment/:style/default-avatar.jpg'
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
