835a7ffdc95df2b0bd9213d11313e3180a9646f9
Thiago Durante Pires <thdurante@gmail.com>
2017-07-18 18:46:37 -0300

    Create and test the 'Relationship' model and it's relations with the 'User' model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,31 @@ class User < ApplicationRecord
                     styles:      { thumb: '160x160#' },
                     default_url: '/images/:attachment/:style/default-avatar.jpg'
 
+  has_many :active_relationships, class_name: 'Relationship',
+           foreign_key: 'follower_id',
+           dependent:   :destroy
+
+  has_many :passive_relationships, class_name: 'Relationship',
+           foreign_key: 'followed_id',
+           dependent:   :destroy
+
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
   validates :name, presence: true
   validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :username, presence: true, uniqueness: true
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
+  def follow(other_user)
+    following << other_user
+  end
+
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  def following?(other_user)
+    following.include?(other_user)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  let(:user1) { create(:user) }
+
+  let(:user2) { create(:user) }
+
   it { should validate_presence_of :name }
 
   it { should validate_presence_of :email }
@@ -19,5 +23,79 @@ RSpec.describe User, type: :model do
     should validate_attachment_content_type(:avatar)
       .allowing('image/png', 'image/gif')
       .rejecting('text/plain', 'text/xml')
+  end
+
+  it do
+    should have_many(:active_relationships)
+               .class_name('Relationship')
+               .with_foreign_key(:follower_id)
+               .dependent(:destroy)
+  end
+
+  it do
+    should have_many(:passive_relationships)
+               .class_name('Relationship')
+               .with_foreign_key(:followed_id)
+               .dependent(:destroy)
+  end
+
+  it do
+    should have_many(:following)
+               .through(:active_relationships)
+               .source(:followed)
+  end
+
+  it do
+    should have_many(:followers)
+               .through(:passive_relationships)
+               .source(:follower)
+  end
+
+  describe '#follow' do
+    describe 'user1 follows user2' do
+      it 'should increment the \'followings\' of user1 by 1' do
+        expect {
+          user1.follow(user2)
+        }.to change(user1.following, :count).by(1)
+      end
+
+      it 'should increment the \'followers\' of user2 by 1' do
+        expect {
+          user1.follow(user2)
+        }.to change(user2.followers, :count).by(1)
+      end
+    end
+  end
+
+  describe '#unfollow' do
+    describe 'user1 unfollows user2' do
+      before(:each) { user1.follow(user2) }
+
+      it 'should decrement the \'followings\' of user1 by -1' do
+        expect {
+          user1.unfollow(user2)
+        }.to change(user1.following, :count).by(-1)
+      end
+
+      it 'should decrement the \'followers\' of user2 by -1' do
+        expect {
+          user1.unfollow(user2)
+        }.to change(user2.followers, :count).by(-1)
+      end
+    end
+  end
+
+  describe '#following?' do
+    describe 'user1 follows user2' do
+      before(:each) { user1.follow(user2) }
+
+      it 'user1.following?(user2) should be true' do
+        expect(user1.following?(user2)).to be_truthy
+      end
+
+      it 'user2.following?(user1) should be false' do
+        expect(user2.following?(user1)).to be_falsey
+      end
+    end
   end
 end
