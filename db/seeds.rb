e6acb62fb53aa7a1f1942fe4beab8755c143534d
Thiago Durante Pires <thdurante@gmail.com>
2017-07-20 08:53:47 -0300

    Random relationships in db/seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,3 +17,13 @@ FactoryGirl.create :user, :with_posts, number_of_posts: rand(3..10),
 8.times do
   FactoryGirl.create :user, :with_avatar, :with_posts, number_of_posts: rand(3..10)
 end
+
+# Random relationships
+User.all.each do |user|
+  following_count = rand(0..9)
+
+  following_count.times do
+    target_user = User.all.sample
+    user.follow target_user unless user == target_user || user.following?(target_user)
+  end
+end
