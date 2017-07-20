f2ebf066bff1bbb1c5410f78c66cf9e0367bfa59
Thiago Durante Pires <thdurante@gmail.com>
2017-07-20 15:41:58 -0300

    Testing NotificationRelayJob

--- spec/jobs/notification_relay_job_spec.rb
+++ spec/jobs/notification_relay_job_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe NotificationRelayJob, type: :job do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user1) { create(:user) }
+
+  let(:user2) { create(:user) }
+
+  let(:notification) { create(:notification, user: user1, recipient: user2, notifiable: user2, action: 'followed') }
+
+  subject(:job) { notification }
+
+  it 'matches with enqueued job' do
+    ActiveJob::Base.queue_adapter = :test
+    expect { subject }.to have_enqueued_job.on_queue('default')
+  end
 end
