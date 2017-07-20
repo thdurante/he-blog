5d55dbd196de0535f104f2e7bd54c5b255a7a5a3
Thiago Durante Pires <thdurante@gmail.com>
2017-07-20 18:26:31 -0300

    Uploading db/seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
 require 'factory_girl_rails'
 
 # Default users
-FactoryGirl.create :user, :with_posts, number_of_posts: rand(3..10),
+FactoryGirl.create :user, :with_avatar, :with_posts, number_of_posts: rand(3..10),
                    name: 'Usuário Teste 1',
                    email: 'teste1@teste.com.br',
                    password: 'teste12345',
                    username: 'teste1'
 
-FactoryGirl.create :user, :with_posts, number_of_posts: rand(3..10),
+FactoryGirl.create :user, :with_avatar, :with_posts, number_of_posts: rand(3..10),
                    name: 'Usuário Teste 2',
                    email: 'teste2@teste.com.br',
                    password: 'teste12345',
