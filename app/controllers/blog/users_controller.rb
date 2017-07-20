d615272ef3b4dd2c4ead0315374279206b92d824
Thiago Durante Pires <thdurante@gmail.com>
2017-07-20 18:19:35 -0300

    Implementing recent_activity in the blog

--- app/controllers/blog/users_controller.rb
+++ app/controllers/blog/users_controller.rb
@@ -1,17 +1,29 @@
 module Blog
   class UsersController < ApplicationController
+    require 'will_paginate/array'
+
     skip_before_action :authenticate_user!
     before_action :set_user, only: [:show]
 
     def show
       @post = Post.new
-      @posts = Post.where(user_id: @user.id).paginate(page: params[:page], per_page: 4)
+      @posts = Post.where(user_id: @user.id).paginate(page: params[:post_page], per_page: 4)
+      fetch_recent_activity
     end
 
     private
 
     def set_user
       @user = User.find(params[:id])
+    end
+
+    def fetch_recent_activity
+      @recent_activity = []
+      @user.following.each do |user|
+        @recent_activity << user.posts
+      end
+      @recent_activity = @recent_activity.flatten.sort! { |a,b| b[:created_at] <=> a[:created_at] }
+      @recent_activity = @recent_activity.paginate(page: params[:recent_activity_page], per_page: 4)
     end
   end
 end
